474787f083dd9c24f3bcfd45f3c949e78688da18
aaamber <lxfbigsmile@gmail.com>
2018-03-24 18:49:46 -0500

    handled logout

--- src/components/App.js
+++ src/components/App.js
@@ -14,10 +14,15 @@ class App extends Component {
     this.setState({isLoggedIn: true});
   }
 
+  handleLogout = () =>{
+    this.setState({isLoggedIn: false});
+    localStorage.removeItem(TOKEN_KEY);
+  }
+
   render() {
     return (
       <div className="App">
-        <Header/>
+        <Header isLoggedIn={this.state.isLoggedIn} handleLogout={this.handleLogout} />
         <Main isLoggedIn={this.state.isLoggedIn} handleLogin={this.handleLogIn}/>
         <p className="App-intro">
         </p>
